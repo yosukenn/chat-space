a37c5f0b8ea020618437e9208eb8f375aa748255
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-01 14:57:33 +0900

    Migrate name column index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801053958) do
+ActiveRecord::Schema.define(version: 20180801055358) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
