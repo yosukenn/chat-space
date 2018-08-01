1ccb2290245a73f9ffaeb82c3b55fdfa71a5041e
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-01 15:14:53 +0900

    Add routing group&&message
    Group: new, create, edit, update
    Message: index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: :index
+  end
   resources :users, only: [:edit, :update]
 end
