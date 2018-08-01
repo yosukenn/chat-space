c66b44a4ae58488ab00f50ff17e0d8cdeb5cc3a3
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-01 14:46:27 +0900

    Edit migration remove _id

--- db/migrate/20180801053958_create_members.rb
+++ db/migrate/20180801053958_create_members.rb
@@ -1,8 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
-      t.references :user_id,    null: false, foreign_key: true
-      t.references :group_id,   null: false, foreign_key: true
+      t.references :user,    null: false, foreign_key: true
+      t.references :group,   null: false, foreign_key: true
       t.timestamps
     end
   end
