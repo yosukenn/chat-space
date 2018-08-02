6e5c4e0a4150c4311d3464e8dc504f72ea775981
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-02 18:53:02 +0900

    Add text save function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,16 @@
 class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
-    Message.create(message_params)
+    message = Message.create(message_params)
+    redirect_to group_messages_path(message.group)
   end
 
   private
   def message_params
-    params.require(:message).permit(:body, :image)
+    params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
