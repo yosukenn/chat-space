62c2412f4ca16176b3bbc02963e2bd59a988996e
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-02 16:54:21 +0900

    Edit migration symbol syntax

--- db/migrate/20180802074920_create_messages.rb
+++ db/migrate/20180802074920_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.text :body
       t.string :image
-      t.references: user,   null: false, foreign_key: true
-      t.references: group,  null: false, foreign_key: true
+      t.references :user,   null: false, foreign_key: true
+      t.references :group,  null: false, foreign_key: true
       t.timestamps
     end
   end
