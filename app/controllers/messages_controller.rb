e12f932108ccc610e04f2266d32143ca92fae68e
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-02 17:13:33 +0900

    Add create action to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,13 @@ class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
   end
+
+  def create
+    Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image)
+  end
 end
