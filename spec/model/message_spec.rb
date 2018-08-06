268108c3ef194c0a00b56d9c3d764dc4273fb3e7
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-06 12:00:40 +0900

    Add test message model is invalid without a user_id

--- spec/model/message_spec.rb
+++ spec/model/message_spec.rb
@@ -28,5 +28,11 @@ describe Message do
       message.valid?
       expect(message.errors[:group]).to include("を入力してください")
     end
+
+    it "is invalid without a user_id" do
+      message = build(:message, user_id: nil)
+      message.valid?
+      expect(message.errors[:user]).to include("を入力してください")
+    end
   end
 end
