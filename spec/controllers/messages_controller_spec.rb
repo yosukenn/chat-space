968176aae0b98c36d61a8e297133fa7d6b41554d
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-06 14:17:23 +0900

    index action test if login_user

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe MessagesContoller do
+describe MessagesController, type: :controller do
   let(:group) { create(:group) }
   let(:user) { create(:user) }
 
@@ -9,6 +9,18 @@ describe MessagesContoller do
       before do
         login user
         get :index, params: { group_id: group.id }
+      end
+
+      it "assigns @message" do
+        expect(assigns(:message)).to be_a_new(Message)
+      end
+
+      it "assigns @group" do
+        expect(assigns(:group)).to eq group
+      end
+
+      it "renders index" do
+        expect(response).to render_template :index
       end
     end
 
