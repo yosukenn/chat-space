f34ddad511dc61c033a73f0d0e9c98e33a1850bf
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-06 14:03:20 +0900

    create index action test describe&context

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe MessagesContoller do
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
+
+  describe '#index' do
+    context 'log in' do
+    end
+
+    context 'not log in' do
+    end
+  end
+end
