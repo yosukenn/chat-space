f63fd4f50b875fc17871240799da5532ad709025
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-06 11:55:42 +0900

    Add test message model is valid with a image

--- spec/model/message_spec.rb
+++ spec/model/message_spec.rb
@@ -7,6 +7,10 @@ describe Message do
       expect(message).to be_valid
     end
 
+    it "is valid with a image" do
+      message = build(:message, body: nil)
+      expect(message).to be_valid
+    end
 
 
   end
