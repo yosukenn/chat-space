f145e696596a322be8646734172784f53147334d
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-07 16:44:32 +0900

    set done method + add html

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,14 +1,34 @@
 $(function() {
+
+function buildHTML(message) {
+  var html = `<div class="content__message__name">
+                ${message.user_name}
+                  </div>
+              <p class="content__message__date">
+                ${message.created_at}
+                  </p>
+              <p class="content__message__text">
+                ${message.body}
+                </p>`
+  return html;
+}
+
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
+    var url = $(this).attr('action')
     $.ajax({
-      url: groups,
-      type: "POST",
+      url: url,
+      type: 'POST',
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.content__message').append(html);
+      $('.content__message-send__content__text').val();
     })
 
 
