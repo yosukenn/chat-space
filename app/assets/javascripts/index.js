8af08e6d82978a70ff0820ef9aad35a3f8271845
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-08 16:50:22 +0900

    define ajax request & .done .fail

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,10 +1,45 @@
 $(function() {
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+                </div>`
+    $('.user-search-result').append(html);
+  }
+
+  function appendNoUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user}</p>
+                </div>`
+    $('.user-search-result').append(html);
+  }
+
   $('.chat-group-form__input').on('keyup', function() {
     var input = $(this).val();
-    console.log(input)
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      $('.user-search-result').empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          appendUser(user);
+        });
+      } else {
+        appendNoUser("一致するユーザーはいません。")
+      }
+    })
+
+    .fail(function() {
+      alert('通信に失敗しました。')
+    })
 
 
   });
-
-
 });
