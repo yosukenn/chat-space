f5bb0bf0e6d1879e77148f9b75957b4b6f4bc941
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-15 20:44:51 +0900

    cansel Fix application name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "ChatSpace"
+set :application, "chat-space"
 set :repo_url, "git@github.com:yosukenn/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
